29ab004d2192465a8be4f497af3a7762e6489eb5
Todd Alexander <60899614+toddalex@users.noreply.github.com>
2020-04-23 15:04:18 -0700

    styling for park display complete

--- client/components/ParkDisplay.jsx
+++ client/components/ParkDisplay.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Container, Typography, Paper, Input, FormControl, AppBar, makeStyles  } from '@material-ui/core';
 import Park from './park.jsx';
+
 
 import '../stylesheets/styles.scss';
  
@@ -45,16 +47,24 @@ class ParkDisplay extends Component {
       //     <li className="parkItem"id='weather'>Weather: {this.props.weather}</li>
       //     <li className="parkItem" id='activities'>Activities: {this.props.activities}</li>
       //   </ul>
-      <div id="parkDisplayContainer" >
-        Annacostia National Park
-        <div id="parkDisplayContainerSmall">
-          <img id="parkDisplayImage" src="https://www.nps.gov/common/uploads/structured_data/3C82A965-1DD8-B71B-0B42F2CD698E11A7.jpg" width='250px'/>
+      <div className="parkContainer">
+        <div id="parkDisplayContainer">
+            <img id="parkDisplayImage" src="https://www.nps.gov/common/uploads/structured_data/3C82A965-1DD8-B71B-0B42F2CD698E11A7.jpg"/>
+          <div className="h1Container">
+            <Typography variant="h1">Anacostia National Park</Typography>
+          </div>
         </div>
-        <div>
-          <ul>
-            <li id='description'>"Welcome to Anacostia Park, your neighborhood national park in the heart of Washington, DC!\nEnjoy exercise along the river trail or relax by the water, Anacostia Park is a breath of fresh air and a space to unwind amid a bustling city."</li>
-            <li id='weather'>"Spring brings mild temperatures and a light breeze along with springtime flower blooms. Summer can be hot and humid. Fall cools down and changing of the seasons brings changing of </li>
-          </ul>
+        <div className="parkDisplayText" id="about">
+          <Typography variant="h4" style={styles.h4}>About     &nbsp;</Typography>
+          <Typography variant="body1" id="description" style={styles.body}>Welcome to Anacostia Park, your neighborhood national park in the heart of Washington, DC!\nEnjoy exercise along the river trail or relax by the water, Anacostia Park is a breath of fresh air and a space to unwind amid a bustling city.</Typography>
+        </div>
+        <div className="parkDisplayText" id="weather">
+          <Typography variant="h4" style={styles.h4}>Weather </Typography>
+          <Typography variant="body1" id="description" style={styles.body}>Spring brings mild temperatures and a light breeze along with springtime flower blooms. Summer can be hot and humid. Fall cools down and changing of the seasons brings changing of </Typography>
+        </div>
+        <div className="parkDisplayText" id="activites">
+          <Typography variant="h4" style={styles.h4}>Activites</Typography>
+          <Typography variant="body1" id="description" style={styles.body}>Hiking, Biking, Extreme Kiting, Sailing, Rowboating, Meth.</Typography>
         </div>
       </div>
     )
@@ -62,3 +72,16 @@ class ParkDisplay extends Component {
 }
 
 export default connect(mapStateToProps, null)(ParkDisplay);
+
+const styles = {
+  h4: {
+    paddingRight: '64px',
+    whiteSpace: 'pre-wrap'
+  },
+  body: {
+    whiteSpace: 'pre-wrap',
+    fontSize: '18px',
+    lineHeight: '30px',
+  },
+}
+
